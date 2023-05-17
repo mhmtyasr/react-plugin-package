7f0931e7eff9943481e42ac7ca05a71104e5477a
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-05-17 23:03:29 +0300

    Nre plugin

--- TestProvider.tsx
+++ TestProvider.tsx
@@ -1,16 +1,14 @@
 import React, { createContext, useState, useContext } from "react";
 
 export interface TestContextType {
-    userName: string;
-    setUserName: (userName: string) => void;
 }
 
 let TestContext = createContext<TestContextType>(null!);
 
 function TestProvider({ children }: { children: React.ReactNode }) {
-    const [testUserName, setTestUserName] = useState(0);
+   
 
-    let value = { testUserName, setTestUserName };
+    let value = {  };
 
     return <TestContext.Provider value={value}>{children}</TestContext.Provider>;
 }
