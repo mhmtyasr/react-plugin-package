bb2c2054c16d4e46391d736823bbb7bffc39c872
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-06-21 21:31:45 +0300

    plugin fixed

--- Cesium/components/Map/index.tsx
+++ Cesium/components/Map/index.tsx
@@ -2,27 +2,25 @@ import * as CesiumWidgets from "@cesium/widgets";
 import * as CesiumEngine from "@cesium/engine";
 import * as React from "react";
 import Viewer from "../Resium/Viewer";
-import LabelCollection from "../Resium/LabelCollection";
-import Label from "../Resium/Label";
 
 const Map = ({ getContextApi }: any) => {
   const { missionData, handleAddMissionData } = React.useContext(
     getContextApi("testPluginWithProvider")
   );
 
+
+  const ref = React.useRef<CesiumWidgets.Viewer>();
+
   const center = CesiumEngine.Cartesian3.fromDegrees(-75.59777, 40.03883);
 
+
+  React.useEffect(() => {
+    debugger;
+  }, []);
+
   return (
-    <Viewer full>
-      <LabelCollection
-        modelMatrix={CesiumEngine.Transforms.eastNorthUpToFixedFrame(center)}
-      >
-        <Label
-          fillColor={CesiumEngine.Color.ORANGE}
-          position={new CesiumEngine.Cartesian3(0.0, 0.0, 0.0)}
-          text="Cesium"
-        />
-      </LabelCollection>
+    <Viewer full ref={ref}>
+     
     </Viewer>
   );
 };
