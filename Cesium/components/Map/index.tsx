5180fe506be3f7c9fe6e5444a32afbeba6d73aa6
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-06-22 00:02:01 +0300

    Fixed

--- Cesium/components/Map/index.tsx
+++ Cesium/components/Map/index.tsx
@@ -15,25 +15,23 @@ const Map = ({ getContextApi }: any) => {
 
   const position = [51.505, -0.09];
   return (
-    <div style={{ height: 500 }}>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {missionData.map((item: any) => {
-          return (
-            <CircleMarker
-              center={[position[0], Math.random() * 100]}
-              pathOptions={{ color: "red" }}
-              radius={20}
-            >
-              <Tooltip>{item}</Tooltip>
-            </CircleMarker>
-          );
-        })}
-      </MapContainer>
-    </div>
+    <MapContainer center={position as any} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {missionData.map((item: any, index: number) => {
+        return (
+          <CircleMarker
+            center={[position[0], position[1] + index * 0.01]}
+            pathOptions={{ color: "red" }}
+            radius={20}
+          >
+            <Tooltip>{item}</Tooltip>
+          </CircleMarker>
+        );
+      })}
+    </MapContainer>
   );
 };
 
