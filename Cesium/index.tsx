0a7432fbfed2eab305fbfad591932d5b3bec0efc
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-06-22 00:11:22 +0300

    Fixed

--- Cesium/index.tsx
+++ Cesium/index.tsx
@@ -1,4 +1,12 @@
 import * as React from "react";
+import {
+  CircleMarker,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Tooltip,
+} from "react-leaflet";
 
 const packageJson = require("./package.json") as any;
 import Map from "./components/Map";
@@ -15,7 +23,29 @@ const Plugin = ({ useAuth, getContextApi }) => {
   const { missionData, handleAddMissionData } = React.useContext(
     getContextApi("testPluginWithProvider")
   );
-  return <Map getContextApi={getContextApi} />;
+  const position = [51.505, -0.09];
+  return (
+    <div>
+      Plugin Leaflet
+      <MapContainer center={position as any} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {missionData.map((item: any, index: number) => {
+          return (
+            <CircleMarker
+              center={[position[0], position[1] + index * 0.01]}
+              pathOptions={{ color: "red" }}
+              radius={20}
+            >
+              <Tooltip>{item}</Tooltip>
+            </CircleMarker>
+          );
+        })}
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Plugin;
