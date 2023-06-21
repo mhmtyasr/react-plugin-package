efba1ad507d1592df654a73e6bf1bc566885c55e
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-06-21 22:29:28 +0300

    Fixed

--- Cesium/components/Map/index.tsx
+++ Cesium/components/Map/index.tsx
@@ -2,6 +2,7 @@ import * as CesiumWidgets from "@cesium/widgets";
 import * as CesiumEngine from "@cesium/engine";
 import * as React from "react";
 import Viewer from "../Resium/Viewer";
+import "cesium/Widgets/widgets.css";
 
 const Map = ({ getContextApi }: any) => {
   const { missionData, handleAddMissionData } = React.useContext(
