f59ca6ea72d1b7a95baa617827cf3433cb0ebe1c
Mehmet Yasir Aktunç <37835086+mhmtyasr@users.noreply.github.com>
2023-06-21 23:28:42 +0300

    Leaflet added

--- Cesium/components/Map/index.tsx
+++ Cesium/components/Map/index.tsx
@@ -1,54 +1,24 @@
-import * as CesiumWidgets from "@cesium/widgets";
-import * as CesiumEngine from "@cesium/engine";
 import * as React from "react";
-import Viewer from "../Resium/Viewer";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const Map = ({ getContextApi }: any) => {
   const { missionData, handleAddMissionData } = React.useContext(
     getContextApi("testPluginWithProvider")
   );
 
-  const ref = React.useRef<CesiumWidgets.Viewer>();
-
-  const center = CesiumEngine.Cartesian3.fromDegrees(-75.59777, 40.03883);
-
-  const bingImage = new CesiumEngine.BingMapsImageryProvider({
-    url: "https://dev.virtualearth.net",
-    key:
-      "hGEV0ElLskiRIlZzgg2F~fhiQv6pgjLrqY2fA81Z5aA~AjSy2id6LLKDtprhaSwoo6awtO6F8NKXtVdGG9IxFAlFVTkrJqkQ7zMckQ9-EYtJ",
-    mapStyle: CesiumEngine.BingMapsStyle.AERIAL,
-  });
-
-  CesiumEngine.Ion.defaultAccessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MzkwZmRiNS1jMTRlLTRiNmYtOGFmYy02Yzc2NGJjM2MzYzkiLCJpZCI6MTQzNTIwLCJpYXQiOjE2ODU2MjAwNTd9.OYjbPf3v1Nt6PCqfW8Fp3ViPluFZw9InO2S4-OCYQwU";
-
-  var cesiumTerrain = new CesiumEngine.CesiumTerrainProvider({
-    url: CesiumEngine.IonResource.fromAssetId(1),
-  });
-
+  const position = [51.505, -0.09];
   return (
-    <Viewer
-      full
-      ref={ref}
-      {...{
-        animation: false,
-        baseLayerPicker: false,
-        geocoder: false,
-        homeButton: false,
-        infoBox: false,
-        navigationInstructionsInitiallyVisible: false,
-        selectionIndicator: false,
-        timeline: false,
-        imageryProvider: bingImage,
-        terrainProvider: cesiumTerrain,
-        targetFrameRate: 15,
-        requestRenderMode: true,
-        fullscreenButton: true,
-        maximumRenderTimeChange: Infinity,
-      }}
-    >
-    
-    </Viewer>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
